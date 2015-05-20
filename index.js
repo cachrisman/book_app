3ee7fba35524f7df4c646fffa335b150d24dd9b9
cachrisman <charlie.chrisman@gmail.com>
2015-05-20 15:09:39 -0700

    setup index.js and views/home.html, installed express and body-parser

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    path = require("path");
+
+var app = express(),
+    views = path.join(__dirname, "views");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+  var homePath = path.join(views, "home.html");
+  res.sendFile(homePath);
+});
+
+app.listen(3000, function () {
+  console.log("STARTING");
+});
+
